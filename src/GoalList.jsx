99562e444497a1dda94faf711d5c211a3fa8d837
Ramadan <daudalaramadan2@gmail.com>
2024-02-23 23:17:46 +0000

    rendering some data into another component

--- src/GoalList.jsx
+++ src/GoalList.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 const GoalList = props => {
-    console.log(props.goals )
+    // console.log(props.goals )
     return(
         
             <ol className="goal-list">
+
+                {props.goals.map(goal =>{
+                    return (
+                    <li key={goal.id}>goal.text</li>
+                )})}
                {/* <li>web technology </li>
                <li>system design and analysis</li>
                <li>discret maths </li> */}
