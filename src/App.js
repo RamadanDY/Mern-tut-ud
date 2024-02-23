ab66b40af341c88ee92c4443cc1b7ba479d75f20
Ramadan <daudalaramadan2@gmail.com>
2024-02-23 09:42:14 +0000

    aligning the frontpage well

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import GoalList from "./GoalList";
+// import { ReactDOM } from "react";
 
-function App() {
+const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "web technology" },
+    { id: "cg2", text: "system design and analysis" },
+    { id: "cg3", text: "discret maths" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+
+      <GoalList goals={courseGoals} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/GoalList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+const GoalList = props => {
+    console.log(props.goals )
+    return(
+        
+            <ol className="goal-list">
+               {/* <li>web technology </li>
+               <li>system design and analysis</li>
+               <li>discret maths </li> */}
+            </ol>
+       
+
+    )
+
+
+}
+
+export default GoalList;
