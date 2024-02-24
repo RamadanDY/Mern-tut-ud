e4da35002139da2aacc0a0071be4e8bb53d48e81
Ramadan <daudalaramadan2@gmail.com>
2024-02-24 18:32:55 +0000

    rerendering the react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import GoalList from "./Components/GoalList/GoalList";
 // import { ReactDOM } from "react";
 import NewGoal from "./Components/GoalList/NewGoal/NewGoal";
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
+    // const courseGoals = [
     { id: "cg1", text: "web technology" },
     { id: "cg2", text: "system design and analysis" },
     { id: "cg3", text: "discret maths" },
-  ];
-  /////a callback function from the parent compo into the child compo
+  ]);
+
+  ////sending data from parent to child
+  /////a callback function from the parent compo into the child compo ie NewGoal as the child component
   const addNewGoalHandler = (newGoal) => {
-    ////we reach out to the courseGoal and push the new goal into the array via props(the props on line 22)
-    courseGoals.push(newGoal);
+    ////we reach out to the courseGoal and push the new goal into the array via props(the props on line 22)and now lets pass it to new goals via props on line 26
+    // courseGoals.push(newGoal);
+    // console.log(courseGoals);
+    setCourseGoals(courseGoals.concat(newGoal));
   };
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      {/* the props ie onAddGoal passes just a pointer to one array meaning our input will be exhibited as one div only */}
+      {/* the props ie onAddGoal passes just a pointer ie addNewGoalHandler to one array meaning our input will be exhibited as one div only */}
+      {/* we pass it on the NewGoal compo because that is the child compo that recieves the data */}
       <NewGoal onAddGoal={addNewGoalHandler} />
 
       <GoalList goals={courseGoals} />
--- src/Components/GoalList/NewGoal/NewGoal.jsx
+++ src/Components/GoalList/NewGoal/NewGoal.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import './NewGoal.css'
 
-
-const NewGoal = () => {
+///// after passing it from the Appjs(ie the parent compo)  into the  child compo we can now recieve it here as props in the function NewGoal as showed below 
+const NewGoal = (props) => {
 
   //// this function passes data from the NewGoal to the Appjs
   const addGoalHandler = (event) =>{
     /// the preventDefault prevent the automatical loading
     event.preventDefault();
-    
 
+    ////// this is a dummy goal or its just to display any number 
     const newGoal = {
       id: Math.random().toString(),
       text: 'My new goal'
     }
-    console.log(newGoal)
+
+    ////instead of logging it into console we can just add the onAddGoal and also add the pass the pointer to a function which is newGoal as my argument 
+    // console.log(newGoal);
+    props.onAddGoal(newGoal);
 
   }
 
